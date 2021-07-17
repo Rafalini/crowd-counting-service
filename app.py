acb4b7b75a4208530022c5e63a2305214cb4a944
rbosko <rbosko@ittouch.pl>
2021-07-17 11:32:35 +0200

    web app 1.0

---
+++ app.py
@@ -0,0 +1,9 @@
+# save this as app.py
+from flask import Flask, escape, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
